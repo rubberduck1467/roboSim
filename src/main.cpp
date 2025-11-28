f049b05b28517db354d64932726a693f3f73c6ed
Jeremy <117963339+rubberduck1467@users.noreply.github.com>
2025-11-27 18:52:07 -0700

    Merge control_loop into main (#1)
    * set up tick clock
    
    * added command line config

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <ratio>
+#include <chrono>
+#include <cstring>
+#include <cstdlib>
 
-int main() 
+struct configInfo {
+        int clockTickRate; // in ticks per second. Calcuated value.
+        int updateRate; // in ticks per second. default is 100.
+
+};
+
+int main(int argc, char **argv) 
 {
-        std::cout << "Hello World!\n";
+        using Clock = std::chrono::steady_clock;
+
+        // config
+        configInfo config;
+
+        config.clockTickRate = Clock::period::den;
+        config.updateRate = 100;
+
+        for (int iii = 0; iii < argc; iii++)
+        {
+                if (!strcmp("--updateRate", argv[iii]))
+                {
+                        config.updateRate = atoi(argv[++iii]);
+                }
+        }
+        
+        //main loop
+        auto lastTime = Clock::now().time_since_epoch().count();
+        while (true) 
+        {
+                auto newTime = Clock::now().time_since_epoch().count();
+                // do an update if enough time has passed
+                if ((config.updateRate*(newTime - lastTime)/config.clockTickRate) == 1)
+                {
+                        std::cerr << "I did an update!\n";
+                        lastTime = Clock::now().time_since_epoch().count();
+                }
+        }
         return 0;
 }
+
